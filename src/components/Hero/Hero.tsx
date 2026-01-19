5a93ee3cf19717a3c3f2058f2636d332679a720e
sufiyan2006 <hhuaweilop@gmail.com>
2026-01-19 13:03:24 +0530

    Update Hero.tsx

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -8,58 +8,74 @@ import githubIcon from '../../assets/github.svg'
 import whatsapp from '../../assets/whatsapp.svg'
 import Hello from '../../assets/Hello.gif'
 import telegram from '../../assets/telegram.svg'
+
 export function Hero() {
   return (
     <Container id="home">
       <div className="hero-text">
         <ScrollAnimation animateIn="fadeInUp">
-          <p>Hello <img src={Hello} alt="Hello" width="20px"/>, I'm</p>
+          <p>Hello <img src={Hello} alt="Hello" width="20px" />, I'm</p>
         </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInUp" delay={0.2 * 1000}>
-          <h1>Sufiyan S</h1>
+
+        <ScrollAnimation animateIn="fadeInUp" delay={200}>
+          <h1>Vinayak Singh</h1>
         </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInUp" delay={0.4 * 1000}>
-          <h3>Data Science</h3>
+
+        <ScrollAnimation animateIn="fadeInUp" delay={400}>
+          <h3>Software Engineer</h3>
         </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInUp" delay={0.6 * 1000}>
-          <p className="small-resume">2nd bsc Data science</p>
+
+        <ScrollAnimation animateIn="fadeInUp" delay={600}>
+          <p className="small-resume">1 Year Experience</p>
         </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInUp" delay={0.8 * 1000}>
+
+        <ScrollAnimation animateIn="fadeInUp" delay={800}>
           <BrowserRouter>
-            <NavHashLink smooth to="#contact" className="button">Contact</NavHashLink>
+            <NavHashLink smooth to="#contact" className="button">
+              Contact
+            </NavHashLink>
           </BrowserRouter>
         </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInUp" delay={1 * 1000}>
-      <div className="social-media"><a
-        href="https://www.linkedin.com/in/sufiyan-undefined"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img src={linkedin} alt="Linkedin" />
-      </a>
-        <a
-          href="https://github.com/CodeVinayak/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={githubIcon} alt="GitHub" />
-        </a>
-        <a
-          href="https://api.whatsapp.com/send/?phone=%2B918098667451&text=Hello+sufiyan+I+found+your+contact+through+portfolio+site.%0A%0A"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={whatsapp} alt="Whatsapp" />
-          <a
-          href="https://t.me/CodeVinayak"
-          target="_blank"
-          rel="noreferrer">
-          <img src={telegram} alt="telegram" />
-        </a></div>
+
+        <ScrollAnimation animateIn="fadeInUp" delay={1000}>
+          <div className="social-media">
+            <a
+              href="https://www.linkedin.com/in/codevinayak"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={linkedin} alt="Linkedin" />
+            </a>
+
+            <a
+              href="https://github.com/CodeVinayak/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={githubIcon} alt="GitHub" />
+            </a>
+
+            <a
+              href="https://api.whatsapp.com/send/?phone=%2B919630576848&text=Hello+Vinayak+I+found+your+contact+through+portfolio+site.%0A%0A"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={whatsapp} alt="Whatsapp" />
+            </a>
+
+            <a
+              href="https://t.me/CodeVinayak"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={telegram} alt="telegram" />
+            </a>
+          </div>
         </ScrollAnimation>
       </div>
+
       <div className="hero-image">
-        <ScrollAnimation animateIn="fadeInRight" delay={1 * 1000}>
+        <ScrollAnimation animateIn="fadeInRight" delay={1000}>
           <img src={Illustration} alt="Ilustração" />
         </ScrollAnimation>
       </div>
